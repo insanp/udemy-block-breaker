eaab62403b951cdff14bd50652bf2c5019afa511
Insan <insan.putranda@gmail.com>
2021-01-23 16:25:39 +0700

    Added singleton game status

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    [SerializeField] int score;
 
     // cached reference
     Level level;
+    GameStatus gameStatus;
 
     void Start()
     {
-        Debug.Log("hai");
         level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameStatus>();
         level.AddCountTotalBlocks();
+        Debug.Log(gameStatus);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         level.DecreaseTotalBlocks();
+        gameStatus.AddScore(score);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     [Range(0.1f, 10f)][SerializeField] float gameSpeed;
-    [SerializeField] int score;
+    [SerializeField] int currentScore;
+
+    [SerializeField] TextMeshProUGUI currentScoreText;
+
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        } else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +33,11 @@ public class GameStatus : MonoBehaviour
     void Update()
     {
         Time.timeScale = gameSpeed;
+        currentScoreText.text = currentScore.ToString();
+    }
+
+    public void AddScore(int score)
+    {
+        currentScore += score;
     }
 }
