066bbd85597aba773a329c3b50f7247d2e8871d7
Insan <insan.putranda@gmail.com>
2021-01-23 22:25:00 +0700

    Added unstuck ball velocity modifier

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,9 +8,11 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1;
     [SerializeField] Vector2 initialVelocity;
     [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float minimumVelocity = 3.0f;
 
     bool hasStarted = false;
     private AudioSource audioSource;
+    private Rigidbody2D ballRB;
 
     // state
     Vector2 paddleToBallVector;
@@ -21,6 +23,7 @@ public class Ball : MonoBehaviour
         paddleToBallVector = transform.position - paddle1.transform.position;
         hasStarted = false;
         audioSource = GetComponent<AudioSource>();
+        ballRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -38,7 +41,7 @@ public class Ball : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             // click left mouse
-            GetComponent<Rigidbody2D>().velocity = initialVelocity;
+            ballRB.velocity = initialVelocity;
             hasStarted = true;
         }
     }
@@ -53,8 +56,52 @@ public class Ball : MonoBehaviour
     {
         if (hasStarted)
         {
+            UnstuckBall();
+
             AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
-            audioSource.PlayOneShot(clip); 
+            audioSource.PlayOneShot(clip);
+        }
+    }
+
+    private void UnstuckBall()
+    {
+        Debug.Log(ballRB.velocity);
+        bool corrected = false;
+        Vector2 newV = ballRB.velocity;
+
+        if (ballRB.velocity.x < minimumVelocity && ballRB.velocity.x > -minimumVelocity)
+        {
+            Debug.Log("Correcting velocity x");
+            if (ballRB.velocity.x > 0f)
+            {
+                newV.Set(minimumVelocity, ballRB.velocity.y);
+            }
+            else
+            {
+                newV.Set(-minimumVelocity, ballRB.velocity.y);
+            }
+
+            corrected = true;
+        }
+
+        if (ballRB.velocity.y < minimumVelocity && ballRB.velocity.y > -minimumVelocity)
+        {
+            Debug.Log("Correcting velocity y");
+            if (ballRB.velocity.y > 0f)
+            {
+                newV.Set(ballRB.velocity.x, minimumVelocity);
+            }
+            else
+            {
+                newV.Set(ballRB.velocity.x, -minimumVelocity);
+            }
+
+            corrected = true;
+        }
+
+        if (corrected)
+        {
+            ballRB.velocity = newV;
         }
     }
 }
